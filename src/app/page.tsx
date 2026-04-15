7479bad8ec04d036986fac5562e138c5ccc87ecd
Douglas Masuku <dmasuku2008@me.com>
2026-04-15 23:21:44 +0100

    fix: force dynamic rendering for root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
 import styles from './page.module.css'
+export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic'
 
 export default async function LandingPage() {
   const supabase = createClient()
